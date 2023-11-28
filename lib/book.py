ac212d73cfb10bc90e2e061de629270af8105c1a
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-28 18:09:42 +0300

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+      def __init__(self, title, page_count=None):
+        self.title = title
+        self._page_count = 0
+        if self._page_count is not None:
+            self.set_page_count(page_count)
+      def get_page_count(self):
+        return self._page_count
+      def set_page_count(self,page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+      def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+      page_count = property(get_page_count, set_page_count) 
+book = Book("And Then There Were None")
+book._page_count = "9p0"
+print(book._page_count)
+book.turn_page()
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size=None):
+        self.condition ="New"
+        self.brand = brand
+        self._size = 0
+        if self._size is not None:
+            self.set_size(size)
+    def get_size(self):
+        return self._size
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        print('Your shoe is as good as new!')
+    size = property(get_size, set_size)
